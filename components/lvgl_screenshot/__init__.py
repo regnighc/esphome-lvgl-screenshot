41bbc5ed9b346936d736073246b463999b6c97dc
regnighc <james.crawford001@gmail.com>
2026-04-02 11:41:26 +0100

    Add LVGL dependency to lvgl_screenshot
    Added dependency for LVGL in lvgl_screenshot component.

--- components/lvgl_screenshot/__init__.py
+++ components/lvgl_screenshot/__init__.py
@@ -2,7 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-# Define the namespace and the C++ class
+DEPENDENCIES = ["lvgl"]
+
 lvgl_screenshot_ns = cg.esphome_ns.namespace("lvgl_screenshot")
 LVGLScreenshot = lvgl_screenshot_ns.class_("LVGLScreenshot", cg.Component)
 
@@ -13,5 +14,3 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    # Ensure LVGL is included in the build
-    cg.add_library("lvgl", None)
