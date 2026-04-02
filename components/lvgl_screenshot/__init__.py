e97bb49ce6006028be96b89197386cc89440ac8c
regnighc <james.crawford001@gmail.com>
2026-04-02 13:24:26 +0100

    Add dependencies and build flags for LVGL
    Add build flags for LVGL snapshot and PSRAM optimization.

--- components/lvgl_screenshot/__init__.py
+++ components/lvgl_screenshot/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
+# Specify that we depend on the 'lvgl' component
 DEPENDENCIES = ["lvgl"]
 
 lvgl_screenshot_ns = cg.esphome_ns.namespace("lvgl_screenshot")
@@ -14,3 +15,8 @@ CONFIG_SCHEMA = cv.Schema({
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
+    
+    # This replaces the need for the 'defines' block in your YAML
+    cg.add_build_flag("-DLV_USE_SNAPSHOT=1")
+    # Force PSRAM optimization flags
+    cg.add_build_flag("-DCONFIG_SPIRAM_USE_MALLOC=1")
